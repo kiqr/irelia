cc2c5a6cb5cc906b9f6216fa32a753efaa6af6cc
Rasmus Kjellberg <2277443+kjellberg@users.noreply.github.com>
2024-03-10 21:14:41 +0100

    Tests for default controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,9 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
-  before_action :authenticate_account!, unless: :devise_controller?
-  before_action :setup_breadcrumb_for_team
+  include Irelia::Controllers::Application
 
   private
-    def setup_breadcrumb_for_team
-      return unless user_signed_in?
-      breadcrumb(current_account.name, "#") if current_account && current_account != current_user.personal_account
-    end
-
     # Redirect to the sign in page after signing out.
     def after_sign_out_path_for(resource_or_scope)
       new_user_session_path
--- test/controllers/dashboard_controller_test.rb
+++ test/controllers/dashboard_controller_test.rb
@@ -3,8 +3,20 @@
 require "test_helper"
 
 class DashboardControllerTest < ActionDispatch::IntegrationTest
-  test "should redirect to authentication page" do
+  test "should redirect to authentication page if not signed in" do
     get root_path
     assert_redirected_to new_user_session_path
+  end
+
+  test "should redirect to onboarding if user has no personal account" do
+    sign_in users(:john)
+    get root_path
+    assert_redirected_to setup_personal_account_path
+  end
+
+  test "should show dashboard if user has team" do
+    sign_in users(:jane)
+    get root_path
+    assert_response :success
   end
 end
