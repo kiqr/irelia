9e634c43f034bcdbfba9ebc86b7a85de488415c9
Rasmus Kjellberg <2277443+kjellberg@users.noreply.github.com>
2024-06-16 12:46:30 +0200

    test: remove old test

--- test/system/components/irelia/navbar_test.rb
+++ test/system/components/irelia/navbar_test.rb
@@ -5,7 +5,6 @@ require "application_system_test_case"
 class Irelia::Navbar::ComponentSystemTest < ApplicationSystemTestCase
   def test_default_preview
     visit("/rails/view_components/irelia/navbar/default")
-
     assert_selector "section.irelia-navbar"
   end
 end
