57c939c71c6f259741cceb0a8cbe865644fcdae2
Rasmus Kjellberg <2277443+kjellberg@users.noreply.github.com>
2024-06-21 17:19:02 +0200

    feat: js for dropdown toggling (alpinejs temporary)

--- app/components/irelia/dropdown/component.rb
+++ app/components/irelia/dropdown/component.rb
@@ -3,23 +3,36 @@
 class Irelia::Dropdown::Component < Irelia::Component
   option :open, default: -> { false }
 
-  renders_one :trigger, types: {
-    button: { renders: Irelia::Button::Component, as: :trigger }
-  }
+  renders_one :trigger, "DropdownTrigger"
 
   renders_many :items, "DropdownItem"
 
   def variants
     class_names(
       "irelia-dropdown",
-      "irelia-dropdown--default": variant == :default,
-      "irelia-dropdown--open": open
+      "irelia-dropdown--default": variant == :default
     )
+  end
+
+  def initialize(**options)
+    super
+
+    @html_options["x-data"] = "{ dropdownOpen: #{open} }"
+  end
+
+  class DropdownTrigger < Irelia::Button::Component
+    def initialize(**options)
+      options[:html_options] ||= {}
+      options[:html_options]["@click"] = "dropdownOpen = true"
+      super(**options)
+    end
   end
 
   class DropdownItem < Irelia::Button::Component
     def initialize(**options)
-      super(size: :sm, variant: :dropdown_item, **options)
+      options[:size] = :sm
+      options[:variant] = :dropdown_item
+      super(**options)
     end
   end
 end
--- lib/irelia/config.rb
+++ lib/irelia/config.rb
@@ -6,5 +6,17 @@ module Irelia
     # Icon pack's to use for the application. This will be autoloaded
     # in the application layout when using the `irelia_head` helper.
     config_accessor :icon_packs, default: [ "font_awesome" ]
+
+    # => Load Poppins Google font.
+    # This will be autoloaded in the application layout when using the `irelia_head` helper.
+    config_accessor :load_poppins, default: true
+
+    # => Load Alpine.js
+    # This will be autoloaded in the application layout when using the `irelia_head` helper.
+    config_accessor :load_alpinejs, default: true
+
+    # => Enable Multi Appearance (Dark & Light Mode)
+    # This will be autoloaded in the application layout when using the `irelia_head` helper.
+    config_accessor :multi_appearance, default: true
   end
 end
