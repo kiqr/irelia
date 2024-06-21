5e720b5c494fd0bfea8a18f1b242c4f271ade786
Rasmus Kjellberg <2277443+kjellberg@users.noreply.github.com>
2024-06-21 21:37:35 +0200

    chore: remove alpinejs

--- lib/irelia/config.rb
+++ lib/irelia/config.rb
@@ -11,10 +11,6 @@ module Irelia
     # This will be autoloaded in the application layout when using the `irelia_head` helper.
     config_accessor :load_poppins, default: true
 
-    # => Load Alpine.js
-    # This will be autoloaded in the application layout when using the `irelia_head` helper.
-    config_accessor :load_alpinejs, default: true
-
     # => Enable Multi Appearance (Dark & Light Mode)
     # This will be autoloaded in the application layout when using the `irelia_head` helper.
     config_accessor :multi_appearance, default: true
