7a9fe9fc29a55d774d5ed9e31afbab410c34ca2f
Rasmus Kjellberg <2277443+kjellberg@users.noreply.github.com>
2024-06-21 10:25:21 +0200

    feat: irelia_button_to helper for Rails

--- app/helpers/irelia/components_helper.rb
+++ app/helpers/irelia/components_helper.rb
@@ -16,5 +16,28 @@ module Irelia
         render component.constantize.new(*args, **kwargs), &block
       end
     end
+
+    def irelia_button_to(name = nil, options = nil, args = nil, &block)
+      args, options = options, name if args.nil?
+      options ||= {}
+      args ||= {}
+      args[:html_options] ||= {}
+      args[:html_options][:class] ||= args[:class]
+
+      component = Irelia::Button::Component.new(skip_tag: true, **args)
+      component_attributes = component.class.dry_initializer.definitions.keys
+
+      if block
+        component.with_content(capture(&block))
+      elsif name != options
+        component.with_content(name)
+      end
+
+      html_options = args.merge(component.html_options)
+
+      button_to(options, html_options.except(*component_attributes)) do
+        render(component)
+      end
+    end
   end
 end
