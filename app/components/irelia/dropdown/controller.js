9a1e273a450cb502a5b5e8aac98a587871e5f7e8
Rasmus Kjellberg <2277443+kjellberg@users.noreply.github.com>
2024-06-23 19:57:27 +0200

    fix: dropdown menu default state

--- app/components/irelia/dropdown/controller.js
+++ app/components/irelia/dropdown/controller.js
@@ -1,3 +1,14 @@
 import Dropdown from "@stimulus-components/dropdown";
 
-export default class extends Dropdown {}
+export default class extends Dropdown {
+  static values = {
+    open: Boolean,
+  };
+
+  connect() {
+    super.connect();
+    if (this.openValue) {
+      this.enter();
+    }
+  }
+}
