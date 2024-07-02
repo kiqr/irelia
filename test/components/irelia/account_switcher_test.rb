86200f2847aa34e3e52418879fc197a2eb46771d
Rasmus Kjellberg <2277443+kjellberg@users.noreply.github.com>
2024-07-02 19:20:44 +0200

    test: fix so test pass

--- test/components/irelia/account_switcher_test.rb
+++ test/components/irelia/account_switcher_test.rb
@@ -4,7 +4,11 @@ require "test_helper"
 
 class Irelia::AccountSwitcher::ComponentTest < ViewComponent::TestCase
   def test_renders
-    render_inline(Irelia::AccountSwitcher::Component.new)
+    render_inline(Irelia::AccountSwitcher::Component.new(
+      name: "John Doe",
+      account_name: "Team X",
+      avatar: "https://example.com/avatar.jpg",
+    ))
     assert_selector "div.irelia-account-switcher"
   end
 end
