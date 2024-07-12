bbbd2aca4c584b472dd4d752ff9041097337d8b5
Rasmus Kjellberg <2277443+kjellberg@users.noreply.github.com>
2024-07-12 13:28:43 +0200

    feat: print breadcrumb trail

--- app/components/irelia/breadcrumbs/component.rb
+++ app/components/irelia/breadcrumbs/component.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class Irelia::Breadcrumbs::Component < Irelia::Component
+  option :breadcrumbs, default: -> { [] }
+
   def variants
     class_names(
       "irelia-breadcrumbs",
