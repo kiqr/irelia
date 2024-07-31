ac59a851a0c93155deda08f040aca5e2b1c3f22a
Rasmus Kjellberg <2277443+kjellberg@users.noreply.github.com>
2024-07-31 20:14:37 +0200

    feat: with_sub_menu for app_shell

--- app/components/irelia/app_shell/component.rb
+++ app/components/irelia/app_shell/component.rb
@@ -3,6 +3,7 @@
 class Irelia::AppShell::Component < Irelia::Component
   renders_one :sidebar, Irelia::Sidebar::Component
   renders_one :breadcrumbs, Irelia::Breadcrumbs::Component
+  renders_one :sub_menu, Irelia::Navigation::Component
 
   def variants
     class_names(
